d77699e0d3cb048dca69986c0fea21f092f7eb22
Pedro Carneiro <pedrocarneirosshen@gmail.com>
2025-02-26 22:21:27 -0300

    Update Ex15.java

--- Incode_exercicios/src/Semana_2/EstruturasRepeticao/DoWhile/Ex15.java
+++ Incode_exercicios/src/Semana_2/EstruturasRepeticao/DoWhile/Ex15.java
@@ -4,25 +4,26 @@ import java.util.Scanner;
 
 public class Ex15 {
     public static void main(String[] args) {
+
         /*
         Criar um programa que pergunte ao usuário quantas
-         vezes ele deseja repetir uma ação, e continue
-         pedindo até ele informar um valor maior que 0.
-         */
+        vezes ele deseja repetir uma ação, e continue
+        pedindo até ele informar um valor maior que 0.
+        */
         Scanner entrada = new Scanner(System.in);
         System.out.print("Insira quantas vezes deseja printar seu nome: ");
         int num = entrada.nextInt();
         String nome = "Pedro";
         int i = 1;
-       do {
-           System.out.println(nome);
-           if (num == i){
+        do {
+            System.out.println(nome);
+            if (num == i){
                 System.out.print("Insira quantas vezes deseja printar seu nome: ");
                 num = entrada.nextInt();
                 i = 1;
-           }
-           i++;
-       }while (num != 0);
+            }
+            i++;
+        }while (num != 0);
         entrada.close();
 
     }
